e302ee0d3115063d0268dea873c040caad05ff09
QlikDinara <66374926+QlikDinara@users.noreply.github.com>
2023-02-21 17:43:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_selected)
